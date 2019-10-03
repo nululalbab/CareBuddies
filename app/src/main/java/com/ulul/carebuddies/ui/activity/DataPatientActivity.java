2d5ebfe2406b0de0a81fb91619b40e401e3390a0
Ulul <nululalbab@gmail.com>
2019-10-04 00:56:38 +0700

    call data patient

--- app/src/main/java/com/ulul/carebuddies/ui/activity/DataPatientActivity.java
+++ app/src/main/java/com/ulul/carebuddies/ui/activity/DataPatientActivity.java
@@ -1,10 +1,18 @@
 package com.ulul.carebuddies.ui.activity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,6 +24,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.ulul.carebuddies.R;
 import com.ulul.carebuddies.model.DataInformation;
+
+import static android.Manifest.permission.CALL_PHONE;
 
 public class DataPatientActivity extends AppCompatActivity {
     @Override
@@ -30,10 +40,11 @@ public class DataPatientActivity extends AppCompatActivity {
 
         final TextView tv_nama_patient = (TextView) findViewById(R.id.tv_nama_patient);
         final TextView tv_alamat_patient = (TextView) findViewById(R.id.tv_alamat_patient);
-        final TextView tv_no_telp_patient = (TextView) findViewById(R.id.no_telp_patient);
+        final TextView tv_no_telp_patient = (TextView) findViewById(R.id.tv_no_telp_patient);
         final TextView tv_ttl = (TextView) findViewById(R.id.tv_ttl);
         final TextView tv_jenis_kelamin = (TextView) findViewById(R.id.tv_jenis_kelamin);
         final TextView tv_sumber_biaya = (TextView) findViewById(R.id.tv_sumber_biaya);
+        final FloatingActionButton fab_call_patient = (FloatingActionButton)findViewById(R.id.fab_call_patient);
         String key = getIntent().getStringExtra("data");
 
         getRefenence.child("user").child(key).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -47,6 +58,22 @@ public class DataPatientActivity extends AppCompatActivity {
                     tv_ttl.setText(profil.getTtl());
                     tv_jenis_kelamin.setText(profil.getJenis_kelamin());
                     tv_sumber_biaya.setText(profil.getSumber_biaya());
+
+                    final String nope = profil.getNo_telp();
+                    fab_call_patient.setOnClickListener(new View.OnClickListener(){
+                        @RequiresApi(api = Build.VERSION_CODES.M)
+                        @Override
+                        public void onClick(View view){
+                            Intent callIntent = new Intent(Intent.ACTION_CALL);
+                            callIntent.setData(Uri.parse("tel:" +nope ));
+
+                            if (ContextCompat.checkSelfPermission(DataPatientActivity.this, CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                                startActivity(callIntent);
+                            } else {
+                                requestPermissions(new String[]{CALL_PHONE}, 1);
+                            }
+                        }
+                    });
 
                 } else {
                     //bus number doesn't exists.
